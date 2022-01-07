fce994c7b5212697c88d03b318d729224455dcc1
vixcity <2091283625@qq.com>
2022-01-07 15:48:17 +0800

    增加删除字符串头尾空格功能以及数据的储存

--- .obsidian/plugins/deploy-content-to-anywhere-main/main.js
+++ .obsidian/plugins/deploy-content-to-anywhere-main/main.js
@@ -71,7 +71,8 @@ class SettingTab extends obsidian.PluginSettingTab {
                 .setPlaceholder("Enter git username")
                 .setValue(config.owner)
                 .onChange((value) => __awaiter(this, void 0, void 0, function* () {
-                console.log("git username: " + value.trim());
+                value = value.trim();
+                console.log("git username: " + value);
                 config.owner = value;
                 yield plugin.saveSettings();
             })));
@@ -84,7 +85,8 @@ class SettingTab extends obsidian.PluginSettingTab {
                 .setPlaceholder("Enter git repoName")
                 .setValue(config.repo)
                 .onChange((value) => __awaiter(this, void 0, void 0, function* () {
-                console.log("git repoName:" + value.trim());
+                value = value.trim();
+                console.log("git repoName:" + value);
                 config.repo = value;
                 yield plugin.saveSettings();
             })));
